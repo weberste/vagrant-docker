85fda7b9afa6253784ada11061c0bb5ffa402344
Stefan Weber <stefan.weber@gmail.com>
2014-05-08 15:03:51 +0200

    Don't get dssh anymore (didn't work). Add docker to vagrant group (to avoid sudoing all the time)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 sudo apt-get -q -y update
 sudo apt-get -q -y --force-yes install lxc-docker git wget curl
-curl -Sso /usr/bin/dssh https://raw.github.com/gak/dotfiles/master/home/bin/dssh
-sudo chmod +x /usr/bin/dssh
+# sudo curl -Sso /usr/bin/dssh https://raw.github.com/gak/dotfiles/master/home/bin/dssh
+# sudo chmod +x /usr/bin/dssh
+
+sudo usermod -aG docker vagrant
